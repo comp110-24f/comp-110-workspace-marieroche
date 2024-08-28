d1ac85a683f4b415069df0e3a10adad2d0629d6c
Marie <mroche1@unc.edu>
2024-08-28 09:34:42 -0400

    CQ00

---
+++ CQs/cq00_functions.py
@@ -0,0 +1,19 @@
+"""My first challenge question in COMP110!"""
+
+__author__ = "730466510"
+
+
+def mimic(message: str) -> str:
+    "take input and repeat it back"
+    return message
+
+
+def main() -> None:
+    "print the result of calling mimic"
+
+
+print(mimic(message=input("What is your message?")))
+
+
+if __name__ == "__main__":
+    main()
